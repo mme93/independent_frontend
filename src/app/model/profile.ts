a3de839bdce5678dc89f747131933eee7beee58d
magantus@googlemail.com <Extrem93!>
2022-07-25 01:42:46 +0200

    add profile interface

---
+++ src/app/model/profile.ts
@@ -0,0 +1,9 @@
+export interface Profile{
+  firstName: string;
+  lastName: string;
+  email: string;
+  country: string;
+  educationLevel: string;
+  generalInfo: string;
+  visibility: boolean;
+}
