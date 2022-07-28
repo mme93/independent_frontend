edaee8ef5be01daa89d4fb57badd0ad080502773
magantus@googlemail.com <Extrem93!>
2022-07-28 22:39:48 +0200

    add founding module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'account',
     loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
   },
+  {
+    path: 'founding',
+    loadChildren: () => import('./pages/founding/founding.module').then( m => m.FoundingPageModule)
+  },
 
 ];
 
